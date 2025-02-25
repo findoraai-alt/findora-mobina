f205337484df533352e272e04206bf1f23b3c992
mysilence1408 <mysilence1408@gmail.com>
2025-02-25 22:06:40 +0330

    first commit

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 
 const ContactForm = () => {
@@ -49,9 +49,9 @@ const ContactForm = () => {
               className=" w-full outline-none border-2 border-[#d7d7d9] dark:bg-[#111828] rounded-lg focus:border-[#5046e5] py-1 px-2"
             ></textarea>
           </div>
-          <button className=" bg-[#5046e5] py-3 text-white rounded-lg font-semibold lg:hover:scale-105 transition-all duration-300 ease-in-out">
-            {"Let's"} Talk
-          </button>
+          <div>
+            <ShinySkeuButton />
+          </div>
         </div>
       </div>
       <motion.div
@@ -74,6 +74,44 @@ const ContactForm = () => {
         }}
         className=" w-32 h-32 md:w-44 md:h-44 lg:w-64 lg:h-64 bg-gradient-to-b from-pink-500 to-purple-500 lg:blur-2xl absolute top-[20%] right-[10%] opacity-15 rounded-full"
       />
+    </div>
+  );
+};
+const ShinySkeuButton = () => {
+  const parentRef = useRef<HTMLDivElement | null>(null);
+  const btnRef = useRef<HTMLButtonElement | null>(null);
+
+  useEffect(() => {
+    btnRef?.current?.addEventListener("mouseover", () => {
+      parentRef.current?.style.setProperty("--size", "250px");
+      parentRef.current?.style.setProperty(
+        "--shineColor",
+        "rgba(255, 255, 255, 0.3)"
+      );
+    });
+
+    btnRef?.current?.addEventListener("mouseleave", () => {
+      parentRef.current?.style.setProperty("--size", "0px");
+      parentRef.current?.style.setProperty(
+        "--shineColor",
+        "rgba(255, 255, 255, 0.0)"
+      );
+    });
+
+    btnRef?.current?.addEventListener("mousemove", (e) => {
+      parentRef.current?.style.setProperty("--x", e.offsetX + "px");
+      parentRef.current?.style.setProperty("--y", e.offsetY + "px");
+    });
+  }, []);
+
+  return (
+    <div ref={parentRef} className="skeuParent">
+      <button
+        ref={btnRef}
+        className=" w-full overflow-hidden font-mono cursor-pointer text-white rounded px-4 py-2 bg-[radial-gradient(100%_100%_at_100%_0%,_#af8bee_0%,_#6903f6_100%)] transition-[box-shadow_0.15s_ease,_transform_0.15s_ease] shadow-md hover:shadow-lg hover:-translate-y-0.5 active:translate-y-0.5 active:shadow-[inset_0px_3px_7px_#6903f6] skeu"
+      >
+        Send
+      </button>
     </div>
   );
 };
