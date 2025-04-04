f2e3856f3c58630b992a7de09a9c7035c853ba38
mysilence1408 <mysilence1408@gmail.com>
2025-04-04 16:01:42 +0330

    finaL change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,19 @@
 import BrowserSearch from "@/components/Home/BrowserSearch";
 import Cards from "@/components/Home/Cards";
 import MovingBoxes from "@/components/Home/MovingBoxes";
-import PreLoader from "@/components/Home/PreLoader";
 import FirstAI from "@/components/Home/FirstAI";
 import { Stats } from "@/components/Home/Stats";
 import TabsFeatures from "@/components/Home/TabFeatures";
 import UseOnPhone from "@/components/Home/UseOnPhone";
 import WhatIsFindora from "@/components/Home/WhatIsFindora";
 import WhyFindora from "@/components/Home/WhyFindora";
+import { useEffect } from "react";
+import PreLoader from "@/components/Home/PreLoader";
 
 export default function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0); // Reset scroll position on navigation
+  }, []);
   return (
     <div>
       <PreLoader />
