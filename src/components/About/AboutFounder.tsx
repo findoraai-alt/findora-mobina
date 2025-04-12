37afbdec337a12f85e2406b4467ec1f8f01988fe
mysilence1408 <mysilence1408@gmail.com>
2025-04-12 21:23:52 +0330

    founder img

--- src/components/About/AboutFounder.tsx
+++ src/components/About/AboutFounder.tsx
@@ -18,16 +18,16 @@ const AboutFounder = () => {
         </h3>
         <div className=" relative">
           <img
-            src="/images/founder2.jpeg"
+            src="/images/founder2.jpg"
             alt="img"
-            className="w-full max-h-[200px] lg:max-h-[350px] 2xl:max-h-[550px] object-cover object-center"
+            className="w-full min-h-[200px] lg:min-h-[350px] 2xl:min-h-[550px] max-h-[200px] lg:max-h-[350px] 2xl:max-h-[550px] object-cover object-center"
           />
           <motion.img
             src="/images/founder1.jpg"
             alt="img"
             ref={ref}
             style={{ opacity }}
-            className="w-full max-h-[200px] lg:max-h-[350px] 2xl:max-h-[550px] object-cover object-center absolute inset-0"
+            className="w-full min-h-[200px] lg:min-h-[350px] 2xl:min-h-[550px] max-h-[200px] lg:max-h-[350px] 2xl:max-h-[550px] object-cover object-center absolute inset-0"
           />
         </div>
         <p className="px-4 md:px-8 text-lg">
