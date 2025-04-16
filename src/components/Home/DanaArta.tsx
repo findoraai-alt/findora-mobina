49e64c6ffaebfcf483658eca0b0a5ee089e7f7ee
mysilence1408 <mysilence1408@gmail.com>
2025-04-16 20:52:15 +0330

    dana and prelude

--- src/components/Home/DanaArta.tsx
+++ src/components/Home/DanaArta.tsx
@@ -5,9 +5,15 @@ const DanaArta = () => {
   return (
     <div className=" pt-20 md:pt-24">
       <div className=" space-y-8">
-        <h3 className=" text-center text-3xl lg:text-4xl font-medium">
-          Smart Tools by Findora
-        </h3>
+        <div className=" px-4 md:px-8 flex flex-col gap-4 text-center ">
+          <h3 className=" text-3xl lg:text-4xl font-medium">
+            Beyond Search: Solutions by Findora
+          </h3>
+          <span className=" text-lg lg:text-xl">
+            Explore our AI-powered tools designed to support truth, mental
+            well-being, and media integrity
+          </span>
+        </div>
         <div className=" flex flex-col lg:flex-row">
           <div
             className=" relative h-[50vh] md:h-[80vh] lg:h-[60vh] w-full lg:w-1/2 p-8 lg:hover:w-3/4 bg-cover bg-center lg:transition-all lg:duration-1000 lg:ease-in-out flex items-center justify-center overflow-hidden"
