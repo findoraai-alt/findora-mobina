85860047edcef61de09ee934cc672efbf05492f5
Findora-Mobina <findora.ai@gmail.com>
2026-05-11 16:08:26 +0330

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       <PreLoader />
       <div className=" bg-[#f0f0fc] dark:bg-[#111828]">
         <WhatIsFindora />
-        {/* <DanaArta /> */}
+        <DanaArta />
         <TabsFeatures />
         <Stats />
         <MovingBoxes />
