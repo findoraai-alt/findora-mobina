aa8fb65c2a03179229749ea62d032ae1fd3c185a
Findora-Mobina <findora.ai@gmail.com>
2026-05-21 19:11:19 +0330

    update diagram

--- src/components/Home/Diagram.tsx
+++ src/components/Home/Diagram.tsx
@@ -56,10 +56,11 @@ interface DiagramProps {
 export default function Diagram({
   findoraLogoUrl = "YOUR_FINDORA_LOGO_URL",
 }: DiagramProps) {
-  const [activeCard, setActiveCard] = useState<number>(0);
+  const [activatedCards, setActivatedCards] = useState<boolean[]>([false, false, false]);
   const [isMobile, setIsMobile] = useState(false);
 
   const cardRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const hasRun = useRef(false);
 
   useEffect(() => {
     const checkMobile = () => {
@@ -73,33 +74,75 @@ export default function Diagram({
       window.removeEventListener("resize", checkMobile);
     };
   }, []);
+  
+   useEffect(() => {
+  if (isMobile) return;
+  if (hasRun.current) return;
 
+  hasRun.current = true;
+
+  const delays = [0, 1800, 3600];
+  const timers: NodeJS.Timeout[] = [];
+
+  delays.forEach((delay, index) => {
+    const t = setTimeout(() => {
+      setActivatedCards((prev) => {
+        const copy = [...prev];
+        copy[index] = true;
+        return copy;
+      });
+    }, delay);
+
+    timers.push(t);
+  });
+
+  return () => timers.forEach(clearTimeout);
+}, [isMobile]);
   useEffect(() => {
+    if (isMobile === null) return;
     if (!isMobile) return;
 
     const observer = new IntersectionObserver(
-      (entries) => {
-        let mostVisibleEntry: IntersectionObserverEntry | null = null;
-
+     (entries: IntersectionObserverEntry[]) => {
         entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            if (
-              !mostVisibleEntry ||
-              entry.intersectionRatio > mostVisibleEntry.intersectionRatio
-            ) {
-              mostVisibleEntry = entry;
-            }
-          }
-        });
+          if (!entry.isIntersecting) return;
 
-        if (mostVisibleEntry) {
+          const target = entry.target as HTMLDivElement;
+
           const index = cardRefs.current.findIndex(
-            (card) => card === mostVisibleEntry!.target
+            (card): card is HTMLDivElement => card === target
           );
-          if (index !== -1) {
-            setActiveCard(index);
-          }
-        }
+
+          if (index === -1) return;
+
+          setActivatedCards((prev) => {
+            if (prev[index]) return prev;
+
+            const copy = [...prev];
+            copy[index] = true;
+            return copy;
+          });
+        });
+      
+    
+      //   useEffect(() => {
+      //   if (isMobile) return;
+
+      //   const delays = [0, 1800, 3600];
+
+      //   delays.forEach((delay, index) => {
+      //     setTimeout(() => {
+      //       setActivatedCards((prev) => {
+      //         if (prev[index]) return prev; // جلوگیری از rerun
+      //         const copy = [...prev];
+      //         copy[index] = true;
+      //         return copy;
+      //       });
+      //     }, delay);
+      //   });
+      // }, [isMobile]);
+
+        
       },
       {
         threshold: [0.35, 0.5, 0.65, 0.8],
@@ -130,6 +173,28 @@ export default function Diagram({
           background: rgba(255,255,255,0.03);
           box-shadow: 0px 20px 120px rgba(0,0,0,0.45);
         }
+          /* استایل پایه برای حالت فعال */
+        .active-mobile {
+          
+          box-shadow: inset 0px 0px 60px 10px color-mix(in srgb, var(--clr), transparent 95%) !important;
+          
+          
+          border-color: color-mix(in srgb, var(--clr), transparent 50%) !important;
+          
+          
+          background: color-mix(in srgb, var(--clr), white 98%) !important;
+          
+          transform: scale(1.01);
+          transition: all 0.6s cubic-bezier(0.22, 1, 0.36, 1) !important;
+        }
+
+        
+        .dark .active-mobile {
+          background: color-mix(in srgb, var(--clr), #111828 92%) !important;
+          box-shadow: inset 0px 0px 80px 20px color-mix(in srgb, var(--clr), transparent 85%) !important;
+          border-color: color-mix(in srgb, var(--clr), transparent 50%) !important;
+        }
+
 
         @keyframes borderPulse {
           0% { border-color: transparent; }
@@ -138,11 +203,9 @@ export default function Diagram({
           70% { border-color: transparent; }
           100% { border-color: transparent; }
         }
-
         .anim {
           animation: borderPulse 6s infinite ease-in-out;
         }
-
         .active-mobile {
           border-color: var(--clr) !important;
           box-shadow: 0px 14px 40px rgba(0,0,0,0.12);
@@ -154,9 +217,9 @@ export default function Diagram({
         }
 
         @media (max-width: 1023px) {
-          .anim {
-            animation: none !important;
-          }
+            .anim {
+          animation: none;
+        }
         }
                 @keyframes sweep {
         0% {
@@ -166,6 +229,15 @@ export default function Diagram({
           transform: translateX(120%);
         }
       }
+          @keyframes shine {
+        0% {
+          transform: translateX(-120%) rotate(25deg);
+        }
+        100% {
+          transform: translateX(120%) rotate(0deg);
+        }
+      }
+    
       `}</style>
 
       {/* TITLE */}
@@ -186,7 +258,7 @@ export default function Diagram({
             cardRefs.current[0] = el;
           }}
           className={`card anim p-4 lg:p-5 w-full lg:w-[350px] shrink-0 ${
-            isMobile && activeCard === 0 ? "active-mobile" : ""
+            isMobile && activatedCards[0] ? "active-mobile" : ""
           }`}
           style={{
             ["--clr" as any]: "#c31069",
@@ -227,7 +299,7 @@ export default function Diagram({
             cardRefs.current[1] = el;
           }}
           className={`card anim p-5 lg:p-6 w-full lg:w-[500px] shrink-0 z-10 ${
-            isMobile && activeCard === 1 ? "active-mobile" : ""
+            isMobile && activatedCards[1] ? "active-mobile" : ""
           }`}
           style={{
             ["--clr" as any]: "#008f7a",
@@ -235,14 +307,22 @@ export default function Diagram({
           }}
         >
           <div className="flex items-top gap-4 mb-6">
-            <div className=" w-14 h-14 rounded-lg flex items-top justify-center shrink-0">
-              
+            <div className="relative w-12 h-12 overflow-hidden">
               <img
                 src={findoraLogoUrl}
                 alt="Findora"
-                className="w-12 h-12 object-contain self-start mt-4"
+                className="w-full h-full object-contain"
+              />
+
+              <div
+                className="absolute inset-0 bg-gradient-to-r from-transparent via-white/60 to-transparent pointer-events-none"
+                style={{
+                  transform: "translateX(-120%) rotate(25deg)",
+                  animation: "shine 4s linear infinite",
+                }}
               />
             </div>
+          
 
             <div>
               <div className="h-[7px] w-[30px] rounded-full mb-2 bg-[#008f7a]" />
@@ -266,7 +346,7 @@ export default function Diagram({
                     <div
                       className="absolute top-0 left-0 h-full w-4 bg-gradient-to-r from-transparent via-white/50 to-transparent pointer-events-none"
                       style={{
-                        animation: "sweep 2.5s linear infinite",
+                        animation: "sweep 5s linear infinite",
                       }}
                     />
                   </div>}
@@ -282,7 +362,7 @@ export default function Diagram({
                     <div
                       className="absolute top-0 left-0 h-full w-4 bg-gradient-to-r from-transparent via-white/50 to-transparent pointer-events-none"
                       style={{
-                        animation: "sweep 2.5s linear infinite",
+                        animation: "sweep 5s linear infinite",
                       }}
                     />
                   </div>
@@ -299,7 +379,7 @@ export default function Diagram({
                     <div
                       className="absolute top-0 left-0 h-full w-4 bg-gradient-to-r from-transparent via-white/50 to-transparent pointer-events-none"
                       style={{
-                        animation: "sweep 2.5s linear infinite",
+                        animation: "sweep 5s linear infinite",
                       }}
                     />
                   </div>}
@@ -315,7 +395,7 @@ export default function Diagram({
                     <div
                       className="absolute top-0 left-0 h-full w-4 bg-gradient-to-r from-transparent via-white/50 to-transparent pointer-events-none"
                       style={{
-                        animation: "sweep 2.5s linear infinite",
+                        animation: "sweep 5s linear infinite",
                       }}
                     />
                   </div>}
@@ -331,7 +411,7 @@ export default function Diagram({
             cardRefs.current[2] = el;
           }}
           className={`card anim p-4 lg:p-5 w-full lg:w-[350px] shrink-0 ${
-            isMobile && activeCard === 2 ? "active-mobile" : ""
+            isMobile && activatedCards[2] ? "active-mobile" : ""
           }`}
           style={{
             ["--clr" as any]: "#7332a1",
@@ -347,7 +427,7 @@ export default function Diagram({
                     <div
                       className="absolute top-0 left-0 h-full w-4 bg-gradient-to-r from-transparent via-white/50 to-transparent pointer-events-none"
                       style={{
-                        animation: "sweep 2.5s linear infinite",
+                        animation: "sweep 5s linear infinite",
                       }}
                     />
                   </div>
